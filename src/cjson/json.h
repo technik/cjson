b31c9d71b287f51bfc19399f45ebf9e5c9672a8c
technik <cjtech90@gmail.com>
2015-05-28 00:09:21 +0200

    Defined the basic json interface.

--- src/cjson/json.h
+++ src/cjson/json.h
@@ -23,3 +23,79 @@
 //----------------------------------------------------------------------------------------------------------------------
 // Simple Json C++ library
 //----------------------------------------------------------------------------------------------------------------------
+#ifndef _CJSON_JSON_H_
+#define _CJSON_JSON_H_
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+namespace cjson {
+
+	/// Json object
+	/// Encapsulates all the functionality to operate with json objects.
+	class Json {
+	public:
+		// ----- Basic construction and destruction -----
+		Json(); ///< Default constructor. Creates an empty json.
+		Json(const Json&); ///< Copy constructor. Performs a deep copy.
+		Json(Json&&); ///< Move constructor.
+		Json& operator=(const Json&); ///< Deep copy assignment.
+		Json& operator=(Json&&); ///< Move asignment.
+		~Json(); ///< Destructor
+
+		// ----- Parsing and generation -----
+		/// param _code a string containing a formated json.
+		void parse	(const char* _code);
+		/// generate a string with the formated content of the json object
+		std::string serialize() const;
+
+		// ----- Useful methods -----
+		void setNull	() const; ///< Reset object to default construction state
+
+		bool isNull		() const;
+		bool isNumber	() const;
+		bool isString	() const;
+		bool isArray	() const;
+		bool isObject	() const;
+
+		// ----- Construction from base types -----
+		Json(bool);
+		Json(int);
+		Json(float);
+		Json(const char*);
+		Json(const std::string&);
+		template<typename T_, template<typename> List_>
+		Json(const List_<T_>& _list); ///< Construct from any type that can be traversed as a vector
+		template<typename Val_, template<typename,typename> Map_, typename Key_ = std::string> ///< Construct from any type that can be traversed as a map
+		Json(const Map_<Key_,Val_>&);
+
+		// ----- Assignment from base types -----
+		Json& operator=(bool);
+		Json& operator=(int);
+		Json& operator=(float);
+		Json& operator=(const char*);
+		Json& operator=(const std::string&);
+		template<typename T_, template<typename> List_>
+		Json& operator=(const List_<T_>& _list); ///< Assign any type that can be traversed as a vector
+		template<typename Val_, template<typename,typename> Map_, typename Key_ = std::string> ///< Assign any type that can be traversed as a map
+		Json& operator=(const Map_<Key_,Val_>&);
+
+		// ----- Conversion to base types -----
+		operator bool	() const;
+		operator int	() const;
+		operator float	() const;
+
+		// ----- Vector like access -----
+		const Json&		operator[]	(size_t) const;
+			  Json&		operator[]	(size_t);
+
+		// ----- Map like access -----
+		const Json&		operator[]	(const std::string&) const;
+			  Json&		operator[]	(const std::string&);
+		bool			contains	(const std::string&) const;
+	};
+
+}	// namespace cjson
+
+#endif // _CJSON_JSON_H_
