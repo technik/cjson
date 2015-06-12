6f1ef28684050be561783b07b311b7f3389d30b2
Bardo91 <19Bardo91@gmail.com>
2015-06-12 14:13:48 +0200

    Added begin and end method to access to iterators.
    Example of new use:
    for (std::pair<std::string, cjson::Json*> pair : dict){
         if(pair.first == "key1"){
              // ....
         }
    }

--- src/cjson/json.h
+++ src/cjson/json.h
@@ -35,6 +35,10 @@ namespace cjson {
 	/// \class Json
 	/// \brief Encapsulates all the functionality to operate with json objects.
 	class Json {
+	private:
+		typedef std::map<std::string, Json*>	Dictionary;
+		typedef std::vector<Json*>			Array;
+
 	public:
 		// ----- Basic construction and destruction -----
 		Json(); ///< Default constructor. Creates an empty json.
@@ -115,6 +119,9 @@ namespace cjson {
 			  Json&		operator[]	(const std::string&);
 		bool			contains	(const std::string&) const;
 
+		Json::Dictionary::iterator	begin();
+		Json::Dictionary::iterator	end();
+
 		// ----- Common methods for array and object -----
 		size_t			size	() const;
 
@@ -122,9 +129,6 @@ namespace cjson {
 		void clear();
 
 	private:
-		typedef std::map<std::string,Json*>	Dictionary;
-		typedef std::vector<Json*>			Array;
-
 		/// Possible types of data
 		enum class DataType {
 			null,
