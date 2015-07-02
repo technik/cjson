901773997ac9cf6abe5f0eabdd44a45951e64fc0
Bardo91 <19Bardo91@gmail.com>
2015-07-02 11:37:26 +0200

    Cleaned and rearranged cjson iterator test.

--- test/iterators/test.cpp
+++ test/iterators/test.cpp
@@ -34,20 +34,40 @@
 using namespace cjson;
 using namespace std;
 
+void foreachTest(Json _json, std::vector<int> _vals);
+
+void specificArrayTest();
+void specificDictTest();
+
 int main(int, const char**)
 {
-	// Testing Array iterators
+
+	// Generic usage.
+	foreachTest({ "[3, 65, 98]" }, { 3, 65, 98 });
+	foreachTest({ "{\"key1\":77, \"key2\":125}" }, { 77, 125 });
+
+	// Test Array iterators
+	specificArrayTest();
+	
+	// Test Dictionary iterator
+	specificDictTest();
+
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+void foreachTest(Json _json, std::vector<int> _vals){
+	int index = 0;
+	for (auto json : _json){
+		assert(int(json) == _vals[index]);
+		index++;
+	}
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+void specificArrayTest(){
 	Json jArray;
 	jArray.parse("[1, 2, 3]");
 	Json::iterator iterArray = jArray.begin();
-	
-	double testArray[3] = { 1, 2, 3 };
-	int index = 0;
-	for (Json json : jArray){
-		assert(int(json)== testArray[index]);
-		index++;
-	}
-
 	assert(*iterArray == 1);
 
 	assert(iterArray->serialize() == "1");
@@ -57,18 +77,19 @@ int main(int, const char**)
 	assert(iterArray->serialize() == "3");
 	++iterArray;
 	assert(iterArray == jArray.end());
-	
-	// Testing Dictionary iterator
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+void specificDictTest(){
 	Json jDictionary;
 	jDictionary.parse((R""({"key1":56, "key2":658})""));
 	Json::iterator iterDict = jDictionary.begin();
 
 	assert(*iterDict == 56);
 
-	assert(iterDict.key() == "key1");
 	assert(jDictionary[iterDict.key()] == 56);
 	++iterDict;
-	assert(iterDict.key() == "key2");
 	assert(jDictionary[iterDict.key()] == 658);
-
+	++iterDict;
+	assert(iterDict == jDictionary.end());
 }
